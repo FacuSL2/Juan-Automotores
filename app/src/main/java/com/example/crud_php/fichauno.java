5d51bb6ebd90bf8910c2cd31cf49c0b408bcee0a
Facu <faculpda22@gmail.com>
2023-05-01 21:43:38 -0300

    Se creó Ficha1 correctamente

--- app/src/main/java/com/example/crud_php/fichauno.java
+++ app/src/main/java/com/example/crud_php/fichauno.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -19,6 +20,7 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.bumptech.glide.Glide;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.HashMap;
@@ -26,7 +28,11 @@ import java.util.Map;
 
 public class fichauno extends AppCompatActivity {
 
-    EditText edId, edFichauno;
+    EditText edId, edModelo, edPatente, edComprador, edKm, edColor, edPrecioinfoautos,edCosto, edValorLista, edFichauno, edPorcinfoautos;
+    ImageView imagentaller;
+    FloatingActionButton siguienteuno;
+
+    TextView modelofuno, patentefuno;
     private ImageButton expandBtn;
     private int initialMaxLines;
     private int position;
@@ -36,7 +42,29 @@ public class fichauno extends AppCompatActivity {
         setContentView(R.layout.activity_fichauno);
 
         edId = findViewById(R.id.id);
+        edModelo = findViewById(R.id.modelo);
+        edPatente = findViewById(R.id.patente);
+        edPrecioinfoautos = findViewById(R.id.precioinfoautos);
+        edValorLista = findViewById(R.id.valorlista);
+        edComprador = findViewById(R.id.compradored);
+        edPorcinfoautos = findViewById(R.id.porcinfoautosed);
+        edKm = findViewById(R.id.kmed);
+        edCosto = findViewById(R.id.costoed);
+        edColor = findViewById(R.id.colored);
         edFichauno = findViewById(R.id.fichaunoed);
+        modelofuno = findViewById(R.id.modelofuno);
+        patentefuno = findViewById(R.id.patentefuno);
+        siguienteuno = findViewById(R.id.siguienteuno);
+        siguienteuno.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(getApplicationContext(),Registro.class)
+                        .putExtra("position",position));
+
+            }
+        });
+
         expandBtn = findViewById(R.id.fichaunoed_expand_btn);
         initialMaxLines = edFichauno.getMaxLines();
 
@@ -55,6 +83,8 @@ public class fichauno extends AppCompatActivity {
             }
         });
 
+        imagentaller = findViewById(R.id.imagenfichauno);
+        // Cargar la imagen en el ImageView desde una URL almacenada en la base de datos
 
 
         Intent intent = getIntent();
@@ -62,8 +92,20 @@ public class fichauno extends AppCompatActivity {
 
 
         edId.setText(MainActivityTaller.employeeArrayList.get(position).getId());
-
+        edModelo.setText(MainActivityTaller.employeeArrayList.get(position).getModelo());
+        edPatente.setText(MainActivityTaller.employeeArrayList.get(position).getPatente());
+        edComprador.setText(MainActivityTaller.employeeArrayList.get(position).getNombre());
+        edKm.setText(MainActivityTaller.employeeArrayList.get(position).getCorreo());
+        edColor.setText(MainActivityTaller.employeeArrayList.get(position).getDireccion());
+        edCosto.setText(MainActivityTaller.employeeArrayList.get(position).getCosto());
+        edPrecioinfoautos.setText(MainActivityTaller.employeeArrayList.get(position).getPrecioinfoautos());
+        edPorcinfoautos.setText(MainActivityTaller.employeeArrayList.get(position).getPorcinfoautos());
+        edValorLista.setText(MainActivityTaller.employeeArrayList.get(position).getValorlista());
         edFichauno.setText(MainActivityTaller.employeeArrayList.get(position).getFichauno());
+        String imageUrl = MainActivityTaller.employeeArrayList.get(position).getImagen();
+        Glide.with(this).load(imageUrl).into(imagentaller);
+        modelofuno.setText("Vehículo: " + MainActivityTaller.employeeArrayList.get(position).getModelo());
+        patentefuno.setText("Patente: " + MainActivityTaller.employeeArrayList.get(position).getPatente());
 
 
 
@@ -73,27 +115,19 @@ public class fichauno extends AppCompatActivity {
 
     }
 
-    public void expandirTexto(View view) {
-        if (edFichauno.getMaxLines() == initialMaxLines) {
-            // Si el EditText no está expandido, lo expande y cambia el ícono del botón a contraer.
-            edFichauno.setMaxLines(Integer.MAX_VALUE);
-            expandBtn.setImageResource(R.drawable.expandir);
-        } else {
-            // Si el EditText está expandido, lo contrae y cambia el ícono del botón a expandir.
-            edFichauno.setMaxLines(initialMaxLines);
-            expandBtn.setImageResource(R.drawable.expandir);
-        }
-    }
 
     public void actualizar(View view) {
         final String id = edId.getText().toString();
-
+        final String comprador = edComprador.getText().toString();
+        final String modelo = edModelo.getText().toString();
+        final String patente = edPatente.getText().toString();
+        final String km = edKm.getText().toString();
+        final String color = edColor.getText().toString();
+        final String precioinfoautos = edPrecioinfoautos.getText().toString();
+        final String porcinfoautos = edPorcinfoautos.getText().toString();
+        final String valorlista = edValorLista.getText().toString();
+        final String costo = edCosto.getText().toString();
         final String fichauno = edFichauno.getText().toString();
-
-
-
-
-
 
 
 
@@ -127,6 +161,15 @@ public class fichauno extends AppCompatActivity {
                 Map<String,String> params = new HashMap<String,String>();
 
                 params.put("id",id);
+                params.put("comprador",comprador);
+                params.put("modelo",modelo);
+                params.put("patente",patente);
+                params.put("km",km);
+                params.put("color",color);
+                params.put("precioinfoautos",precioinfoautos);
+                params.put("porcinfoautos",porcinfoautos);
+                params.put("costo",costo);
+                params.put("valorlista",valorlista);
                 params.put("fichauno",fichauno);
 
 
